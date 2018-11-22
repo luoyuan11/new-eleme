8bf56fa6d5eaaa744bdcda378932f6bfdab8a8be
luoyuan <luoyuan77@outlook.com>
2018-11-22 16:39:01 +0800

    antd mobile and antd的css

--- config-overrides.js
+++ config-overrides.js
@@ -1,9 +1,6 @@
 const { injectBabelPlugin } = require('react-app-rewired');
 module.exports = (config, env) => {
-  config = injectBabelPlugin(['import', { libraryName: 'antd-mobile', style: 'css' }], config);
+  config = injectBabelPlugin(['import', { libraryName: 'antd-mobile', style: 'css' }, 'antd-mobile'], config);
+  config = injectBabelPlugin(['import', { libraryName: 'antd', style: 'css' }, 'antd'], config)
   return config;
-}
-module.exports = (configs, env) => {
-  configs = injectBabelPlugin(['import', { libraryName: 'antd', style: 'css' }], configs);
-  return configs;
 }
